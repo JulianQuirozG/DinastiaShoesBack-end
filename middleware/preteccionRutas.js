3e600de1ccc1c4c4fb0fda2469bb1e3b137c0826
JulianQuiroz1152029 <iscalajul@gmail.com>
2024-05-03 23:42:48 -0500

    Cambios de middlewareeew

--- middleware/preteccionRutas.js
+++ middleware/preteccionRutas.js
@@ -4,20 +4,20 @@ require('dotenv').config();
 const protegerRuta = (usuariosPermitidos) => async (req, res, next) => {
     try {
         const authHeader = req.headers['authorization'];
-        if (!authHeader) throw res.status(401).json({ error: 'No hay token, acceso no autorizado' });
+        if (!authHeader) return res.status(401).json({ error: 'No hay token, acceso no autorizado' });
 
         jwt.verify(authHeader, process.env.JWT_PASS, (err, decoded) => {
-            if (err) throw res.status(403).json({ error: 'Token inválido' });
+            if (err) return res.status(403).json({ error: 'Token inválido' });
 
             const user = decoded.tipo;
             console.log(user);
-            if (!usuariosPermitidos.includes(user)) throw res.status(403).json({ error: 'No cuenta con los permisos suficientes para esta función.' });
+            if (!usuariosPermitidos.includes(user)) return res.status(403).json({ error: 'No cuenta con los permisos suficientes para esta función.' });
             req.user = decoded;
             next();
         });
     }
     catch (error) {
-        next(error)
+        console.error(error)
     }
 
 }
