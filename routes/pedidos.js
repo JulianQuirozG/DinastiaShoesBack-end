7a0a4efbbe6169ccc5d3fe0bdeee9437820d4423
JulianQuiroz1152029 <iscalajul@gmail.com>
2024-05-04 00:24:13 -0500

    Cambios de middlewareeewww

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -11,7 +11,7 @@ const app= express()
 router.get('/listar', protegerRuta(["A","E"]), pedidoController.obtenerPedido);
 router.get('/listarUsuario/:cedula', protegerRuta(["A","E","C"]), pedidoController.obtenerPedidosPorUsuario);
 router.get('/obtener/:id', protegerRuta(["A","E","C"]), pedidoController.obtenerUnPedido);
-router.post('/crear',protegerRuta(["C"]), upload.fields([{ name: 'comprobar', maxCount: 1 }]), pedidoController.crearPedido);
+router.post('/crear', upload.fields([{ name: 'comprobar', maxCount: 1 }]), pedidoController.crearPedido);
 router.delete('/eliminar/:id', protegerRuta(["A","E"]), pedidoController.eliminarPedido);
 router.post('/actualizar', protegerRuta(["A","E"]),pedidoController.actualizarPedidoId);
 
