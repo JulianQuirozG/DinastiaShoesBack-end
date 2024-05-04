2b7eeb9f9c6d80a5147ce58bbc91980b8d61f96d
JulianQuiroz1152029 <iscalajul@gmail.com>
2024-05-03 23:37:30 -0500

    Cambios de middlewaree

--- middleware/preteccionRutas.js
+++ middleware/preteccionRutas.js
@@ -1,20 +1,37 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const protegerRuta = (usuariosPermitidos) => async (req, res, next)=>{
+const protegerRuta = (usuariosPermitidos) => async (req, res, next) => {
+    try {
+        const authHeader = req.headers['authorization'];
+        if (!authHeader) {
+            const error = new Error('No hay token, acceso no autorizado');
+            error.status = 401;
+            throw error;
+        }
 
-    const authHeader = req.headers['authorization'];
-    if(!authHeader) return res.status(401).json({ error: 'No hay token, acceso no autorizado' });
+        jwt.verify(authHeader, process.env.JWT_PASS, (err, decoded) => {
+            if (err) {
+                const error = new Error('Token inválido');
+                error.status = 403;
+                throw error;
+            }
 
-    jwt.verify(authHeader, process.env.JWT_PASS,(err, decoded) =>{
-        if(err) return res.status(403).json({ error: 'Token inválido' });
-        
-        const user = decoded.tipo;
-        console.log(user);
-        if (!usuariosPermitidos.includes(user)) return res.status(403).json({ error: 'No cuenta con los permisos suficientes para esta función.' });
-        req.user=decoded;
-        next();
-    });
+            const user = decoded.tipo;
+            console.log(user);
+            if (!usuariosPermitidos.includes(user)) {
+                const error = new Error('Usuario no permitido');
+                error.status = 403;
+                throw error;
+            }
+            req.user = decoded;
+            next();
+        });
+    }
+    catch (error) {
+        next(error.message);
+    }
+
 }
 
 module.exports = protegerRuta;
