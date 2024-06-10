e02a1496f5585adcb029dc607de1a89e5efbcb1c
JulianQuiroz1152029 <iscalajul@gmail.com>
2024-06-10 15:09:42 -0500

    Hora de colombia

--- config/database.js
+++ config/database.js
@@ -9,7 +9,7 @@ const sequelize  = new Sequelize(
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
       dialect: 'mysql',
-      timezone: "-03:00"
+      timezone: "-05:00"
     }
 );
 
