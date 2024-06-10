d8ef08e963e85f58a8b7fd66c8928548f2a35100
JulianQuiroz1152029 <iscalajul@gmail.com>
2024-06-10 03:20:47 -0500

    cambios ultimos si o si

--- config/database.js
+++ config/database.js
@@ -9,7 +9,7 @@ const sequelize  = new Sequelize(
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
       dialect: 'mysql',
-      timezone: "-10:00"
+      timezone: "-12:00"
     }
 );
 
