89c4aa5f61d7681267a0cf71feab5082871956ee
JulianQuirozG <75459275+JulianQuirozG@users.noreply.github.com>
2025-04-07 18:14:44 -0500

    Update server.js

--- server.js
+++ server.js
@@ -45,13 +45,6 @@ app.use('/carrito', carritoRouter);
 app.use('/pedido', pedidoRouter);
 
 app.listen(port, () => {
-    console.log(`server on port ${port}`);
-    console.log(1);
-    console.log("#######################");
-    console.log(process.env.DB_HOST);
-    console.log(process.env.DB_PORT);
-    console.log(process.env.DB_USER);
-    console.log(process.env.DB_PASSWORD);
-    console.log(process.env.DB_DATABASE);
+    console.log(`server on port ${port} ${process.env.DB_HOST} ${process.env.DB_PORT} ${process.env.DB_USER} ${process.env.DB_PASSWORD}  ${process.env.DB_DATABASE}`);
 })
 
